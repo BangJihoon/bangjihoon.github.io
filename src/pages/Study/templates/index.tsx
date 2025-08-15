6c946feb5730e52c9c836ae3b469382d6b2caebc
Bang Jihoon <jihoon289@naver.com>
2025-08-15 21:47:07 +0900

    refactor: extract timeline section

--- src/pages/Study/templates/index.tsx
+++ src/pages/Study/templates/index.tsx
@@ -1,64 +1,18 @@
-import Timeline from '@mui/lab/Timeline'
-import TimelineConnector from '@mui/lab/TimelineConnector'
-import TimelineContent from '@mui/lab/TimelineContent'
-import TimelineDot from '@mui/lab/TimelineDot'
-import TimelineItem from '@mui/lab/TimelineItem'
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent'
-import TimelineSeparator from '@mui/lab/TimelineSeparator'
-import moment from 'moment'
-import React, { useRef } from 'react'
+import React from 'react'
 
-import Text from '../../../components/atom/Text'
-import ResumeCard from '../../../components/molecules/ContentElements/ResumeCard'
+import TimelineSection from '../../../components/TimelineSection'
 import useRootData from '../../../hooks/useRootData'
-import stylesDesktop from './Desktop.module.scss'
-import stylesMobile from './Mobile.module.scss'
 
 interface StudyTemplateProps {
   style?: React.CSSProperties
 }
 
 const App: React.FunctionComponent<StudyTemplateProps> = ({ style }: StudyTemplateProps) => {
-  const { isDesktopView, studyArrayInLatestOrder } = useRootData(({ appStore }) => ({
-    isDesktopView: appStore.isDesktopView,
+  const { studyArrayInLatestOrder } = useRootData(({ appStore }) => ({
     studyArrayInLatestOrder: appStore.studyArrayInLatestOrder,
   }))
 
-  const styles = isDesktopView ? stylesDesktop : stylesMobile
-  const containerRef = useRef(null)
-
-  return (
-    <div ref={containerRef} style={style} className={styles.container}>
-      <Text className={styles.titleText} text="Side Study" />
-
-      <Timeline className={styles.timelineArea} position={isDesktopView ? 'alternate' : null}>
-        {studyArrayInLatestOrder.map((item, index) => {
-          const { startAt, endAt } = item
-
-          return (
-            <TimelineItem key={index}>
-              <TimelineOppositeContent className={styles.dateText}>
-                {`${startAt} -   ${endAt}`}
-              </TimelineOppositeContent>
-              <TimelineSeparator>
-                <TimelineDot />
-                <TimelineConnector />
-              </TimelineSeparator>
-              <TimelineContent>
-                <ResumeCard
-                  style={{
-                    float: isDesktopView && index % 2 ? 'right' : 'left',
-                  }}
-                  isDesktopView={isDesktopView}
-                  resumeObject={item}
-                ></ResumeCard>
-              </TimelineContent>
-            </TimelineItem>
-          )
-        })}
-      </Timeline>
-    </div>
-  )
+  return <TimelineSection style={style} title="Side Study" items={studyArrayInLatestOrder} />
 }
 
 export default App
